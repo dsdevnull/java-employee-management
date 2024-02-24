a0feb0b6503e98e11e9a7b2943428685998da08b
Duncan Squires <dsdevnull@gmail.com>
2024-02-24 10:36:09 -0500

    intial commit

---
+++ src/main/java/squires/projects/javaemployeemanagement/JavaEmployeeManagementApplication.java
@@ -0,0 +1,13 @@
+package squires.projects.javaemployeemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEmployeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEmployeeManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/squires/projects/javaemployeemanagement/JavaEmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package squires.projects.javaemployeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
